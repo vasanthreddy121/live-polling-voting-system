93b96c263ae2f89772327360e79e7a34178ddd5a
vasanthreddy121 <reddyvk174@gmail.com>
2026-03-19 10:04:06 +0530

    Refactor SRN and voting input handling

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
 import socket
 import re
 
-SERVER_IP = "192.168.1.8"   # change to server IP if different machine
+SERVER_IP = "127.0.0.1"   # change to server IP for different laptops
 PORT = 5000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# SRN validation pattern
 pattern = r"^PES2UG\d{2}[A-Z]{2}\d{3}$"
 
 print("------ LIVE POLL ------\n")
@@ -16,30 +15,34 @@ print("1. Linux")
 print("2. Windows")
 print("3. Mac\n")
 
-# get SRN
-while True:
-    srn = input("Enter your SRN: ").upper()
+# sequence number
+seq = 1
 
-    if re.match(pattern, srn):
-        break
-    else:
-        print("Invalid SRN format. Example: PES2UG25CS829\n")
-
-# vote input
 while True:
-    try:
-        vote = int(input("Enter your vote (1-3): "))
-        if vote in [1,2,3]:
+    # SRN input
+    while True:
+        srn = input("Enter your SRN: ").upper()
+        if re.match(pattern, srn):
             break
-    except:
-        pass
+        else:
+            print("Invalid SRN format. Example: PES2UG25CS829\n")
 
-    print("Invalid vote. Try again.")
+    # vote input
+    while True:
+        try:
+            vote = int(input("Enter your vote (1-3): "))
+            if vote in [1, 2, 3]:
+                break
+        except:
+            pass
+        print("Invalid vote. Try again.")
 
-# create packet
-message = f"VOTE|{srn}|{vote}"
+    # create packet
+    message = f"VOTE|{srn}|{vote}|{seq}"
 
-# send vote
-sock.sendto(message.encode(), (SERVER_IP, PORT))
+    # send packet
+    sock.sendto(message.encode(), (SERVER_IP, PORT))
 
-print("\nVote sent successfully!")
+    print("Vote sent successfully!\n")
+
+    seq += 1
